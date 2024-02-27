37e89c3a63bba96843e398aa30740eced1b0f68c
aliciahayashi <aliciahayashi04@gmail.com>
2024-02-27 16:50:30 +1100

    Finally got generation to work, looking into disabling the trigger once player pass through it once.

--- PawPainters/Assets/Scripts/WallTrigger.cs
+++ PawPainters/Assets/Scripts/WallTrigger.cs
@@ -1,19 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WallTrigger : MonoBehaviour
 {
     public GameObject wallSection;
+    public GameObject[] section;
+    public int secNum;
+    public bool alreadyTriggered = false;
+    public GameObject box2DTrigger;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("WallTrigger"))
         {
-            collision.GetComponent<BoxCollider2D>().enabled = false;
+            if(!alreadyTriggered)
+            {
+                secNum = UnityEngine.Random.Range(0, 3);
+                Instantiate(section[secNum], new Vector3(0, 48, 0), Quaternion.identity);
+                Debug.Log("Trigger Hit!");
+                
+            }
 
-            Instantiate(wallSection, new Vector3(0, 15, 0), Quaternion.identity);
-            Debug.Log("Trigger Hit!");
+
+        }
+            //collision.GetComponent<BoxCollider2D>().enabled = false;
+
+           
         }
     }
 }
