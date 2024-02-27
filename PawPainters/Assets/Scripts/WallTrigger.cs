1ea586cbd5c1c360bf25fa8dfbd84fe7487da13d
aliciahayashi <aliciahayashi04@gmail.com>
2024-02-27 16:18:17 +1100

    Fixed wall trigger

--- PawPainters/Assets/Scripts/WallTrigger.cs
+++ PawPainters/Assets/Scripts/WallTrigger.cs
@@ -6,10 +6,12 @@ public class WallTrigger : MonoBehaviour
 {
     public GameObject wallSection;
 
-    private void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (other.gameObject.CompareTag("WallTrigger"))
+        if (collision.gameObject.CompareTag("WallTrigger"))
         {
+            collision.GetComponent<BoxCollider2D>().enabled = false;
+
             Instantiate(wallSection, new Vector3(0, 15, 0), Quaternion.identity);
             Debug.Log("Trigger Hit!");
         }
